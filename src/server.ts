e0bb9e379ea306e40925cc308f176e58c9afa5b0
Saad Srabon <saadsrabon2@gmail.com>
2026-02-07 13:53:03 +0600

    data updated

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import express, { Application, Request, Response } from 'express';
-
-const app: Application = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
-
+import app from './app';
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
