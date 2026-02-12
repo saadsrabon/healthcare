0d675dbe33e635a9f74ce8558d43602e40bb6886
Saad Srabon <saadsrabon2@gmail.com>
2026-02-12 20:06:44 +0600

    routes done

--- src/app/routes/index.routes.ts
+++ src/app/routes/index.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { SpecialityRoutes } from "../modules/specialities/specialities.routes";
+import { AuthRoutes } from "../modules/auth/auth.routes";
 
 const router = Router();
-
+router.use("/auth", AuthRoutes)
 router.use("/specialties", SpecialityRoutes)
 
 
