8e4479fab03c46448f28461bb15113fd2bc3a49d
saadsrabon <saadsrabon2@gmail.com>
2026-02-25 08:24:43 +0600

    zod validation added

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,7 +1,55 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import z from 'zod';
 const router = express.Router();
 
-router.post('/create-doctor', UserController.createDoctor);
+// export interface ICreateDoctorPayload {
+//     password: string;
+//     doctor: {
+//         name: string;
+//         email: string;
+//         profilePhoto?: string;
+//         contactNumber?: string;
+//         address?: string;
+//         registrationNumber: string;
+//         experience?: number;
+//         gender: Gender;
+//         appointmentFee: number;
+//         qualification: string;
+//         currentWorkingPlace: string;
+//         designation: string;
+//     }
+//     specialties: string[];
+// }
+// zod validation for create doctor
+const createDoctorZodSchema = z.object({
+    password: z.string("Password is required").min(6,"Password must be at least 6 characters long"),
+    doctor: z.object({
+        name: z.string("Name is required").min(3,"Name must be at least 3 characters long"),
+        email: z.email("Invalid email"),
+        profilePhoto: z.string("").optional(),
+        contactNumber: z.string().optional(),
+        address: z.string().optional(),
+        registrationNumber: z.string(),
+        experience: z.number().optional(),
+        gender: z.enum(["MALE", "FEMALE", "OTHER"]),
+        appointmentFee: z.number(),
+        qualification: z.string(),
+        currentWorkingPlace: z.string(),
+        designation: z.string(),
+    }),
+    specialties: z.array(z.string()),
+})
+
+router.post('/create-doctor',(req,res,next) =>{
+   
+    const {error,success,data} = createDoctorZodSchema.safeParse(req.body);
+    if(!success){
+      next(error)
+    }
+    req.body = data;
+
+    next()
+}, UserController.createDoctor);
 
 export const UserRoutes = router;
